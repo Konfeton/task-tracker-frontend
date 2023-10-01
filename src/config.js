ba094b16e336141b4d1ab731733c2fa83749f34b
Polina Arkhipenko <polinar977@gmail.com>
2023-10-01 23:24:48 +0300

    Added disable SameSite

--- src/config.js
+++ src/config.js
@@ -2,3 +2,12 @@ module.exports = global.config = {
     BASE_URL : "http://34.116.165.172:8085/time-tracker"
     // other global config variables you wish
 };
+
+module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome' || browser.name === 'edge') {
+            launchOptions.args.push('--disable-features=SameSiteByDefaultCookies') 
+            return launchOptions
+        }
+    })
+}
