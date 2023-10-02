e1fab2f60b383eea486d02e95cb1659033f9f011
Dmitry Dulko <dimadulko007@gmail.com>
2023-10-02 17:11:43 +0300

    added button show users

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -17,9 +17,11 @@ function WelcomePage(props) {
         <div>
             <div className="mt-5 pt-5 row  d-inline-block justify-content-between">
                 <div className="">
-                    <Link to={"/notes/create"} className="btn btn-primary mb-3 me-5">Create note</Link>
+                    <Link to={"/notes/create"} className="btn btn-primary  me-5">Create note</Link>
 
-                    <Link to={"/notes"} className="btn btn-primary mb-3 ms-5">Check notes</Link>
+                    <Link to={"/notes"} className="btn btn-primary  me-5 ms-5">Check notes</Link>
+
+                    <Link className="btn btn-primary ms-5" to="/users">Show users</Link>
                 </div>
             </div>
 
